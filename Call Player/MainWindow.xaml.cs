966b7084c2468c8963f47756f9a3c510ad51db73
Saleem Ali <TechFreak1o1@live.com>
2022-07-07 19:30:42 -0400

    Add project files.

---
+++ Call Player/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Call_Player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class CallInfo
+        {
+            public string FileName { get; set; }
+
+            public string Date { get; set; }
+
+            public string Caller { get; set; }
+
+            public string CallDate { get; set; }
+
+            public string Calltime { get; set; }
+
+            public string FullPath { get; set; }
+        }
+
+    }
+}
